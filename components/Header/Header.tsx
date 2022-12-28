562ddec8897ec74105d5ef925e27edf7138fe043
wojciech-turek <contact@wojciechturek.com>
2022-12-28 19:36:41 +0100

    Update styling of menu items

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -5,6 +5,9 @@ import { SlMenu } from "react-icons/sl";
 import FlexibleContainer from "components/FlexibleContainer/FlexibleContainer";
 import { truncateAddress } from "utils/client/truncateAddress";
 import styles from "./Header.module.scss";
+import Link from "next/link";
+import classNames from "classnames";
+import Container from "components/Container/Container";
 const Drawer = dynamic(() => import("components/Drawer/Drawer"));
 
 const Header = () => {
@@ -17,14 +20,35 @@ const Header = () => {
     connect({ connector: connectors[0] });
   };
 
+  const navigationItems = [
+    {
+      name: "Home",
+      link: "/",
+    },
+    {
+      name: "Create",
+      link: "/create",
+    },
+    {
+      name: "My Tokens",
+      link: "/my-tokens",
+    },
+  ];
+
   return (
     <>
       <header className={styles.bar}>
         <FlexibleContainer>
           <div className={styles.content}>
-            <div className={styles.logoText}>TokenHatchery</div>
+            <div className={styles.logoText}>
+              <Link href="/">TokenHatchery</Link>
+            </div>
             <div className={styles.desktopRight}>
-              <button>Create new token contract</button>
+              {navigationItems.map((item) => (
+                <div className={styles.menuItem} key={item.name}>
+                  <Link href={item.link}>{item.name}</Link>
+                </div>
+              ))}
               {!isConnected ? (
                 <button onClick={() => connectWallet()}>Connect Wallet</button>
               ) : (
@@ -41,14 +65,16 @@ const Header = () => {
         </FlexibleContainer>
       </header>
       <Drawer open={drawerOpen} onClose={() => setDrawerOpen(false)}>
-        <>
-          <div>abc</div>
-          <div>abc</div>
-          <div>abc</div>
-          <div>abc</div>
-          <div>abc</div>
-          <div>abc</div>
-        </>
+        <Container direction="column" gap={16}>
+          {navigationItems.map((item) => (
+            <div
+              className={classNames(styles.menuItem, styles.dark)}
+              key={item.name}
+            >
+              <Link href={item.link}>{item.name}</Link>
+            </div>
+          ))}
+        </Container>
       </Drawer>
     </>
   );
