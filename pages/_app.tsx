3162dd3a1ce287d32e36080b11b3051dabbef499
wojciech-turek <contact@wojciechturek.com>
2022-12-28 20:56:53 +0100

    Add more networks and conditional button for minting progress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,16 @@ import "styles/variables.scss";
 import "styles/global.scss";
 import type { AppProps } from "next/app";
 import { WagmiConfig, createClient, configureChains, goerli } from "wagmi";
+import { bsc, bscTestnet, mainnet } from "wagmi/chains";
 import { InjectedConnector } from "wagmi/connectors/injected";
 import { publicProvider } from "wagmi/providers/public";
 import Layout from "components/Layout/Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const { chains, provider } = configureChains([goerli], [publicProvider()]);
+  const { chains, provider } = configureChains(
+    [mainnet, goerli, bsc, bscTestnet],
+    [publicProvider()]
+  );
 
   const client = createClient({
     connectors: [new InjectedConnector({ chains })],
--- pages/create/erc20.tsx
+++ pages/create/erc20.tsx
@@ -1,9 +1,20 @@
 import Accordion from "components/Accordion/Accordion";
+import Container from "components/Container/Container";
 import FlexibleContainer from "components/FlexibleContainer/FlexibleContainer";
+import NetworkSelect from "components/NetworkSelect/NetworkSelect";
+import BodyText from "components/Typography/BodyText/BodyText";
 import Heading from "components/Typography/Heading/Heading";
+import WalletConnect from "components/WalletConnect/WalletConnect";
 import React from "react";
 
 const Erc20 = () => {
+  const [connectionEstablished, setConnectionEstablished] =
+    React.useState<boolean>(false);
+
+  const handleConnected = (val: boolean) => {
+    setConnectionEstablished(val);
+  };
+
   return (
     <FlexibleContainer>
       <Heading>ERC20 Creator</Heading>
@@ -12,8 +23,10 @@ const Erc20 = () => {
           <Accordion.Header>
             Connect your wallet and select network
           </Accordion.Header>
-          <Accordion.Body>
+          <Accordion.Body canMoveNext={connectionEstablished}>
             <p>Chose one of the methods below to connect your wallet</p>
+            <WalletConnect />
+            <NetworkSelect setConnected={handleConnected} />
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item>
@@ -29,6 +42,16 @@ const Erc20 = () => {
           <Accordion.Body>Body1</Accordion.Body>
         </Accordion.Item>
       </Accordion>
+      <Container>
+        <BodyText>
+          Do you have any suggestions or feedback to improve this tool?{" "}
+          <a href="mailto: contact@wojciechturek.com">
+            <span style={{ fontWeight: "bold" }}>
+              Click here to contact us.
+            </span>
+          </a>
+        </BodyText>
+      </Container>
     </FlexibleContainer>
   );
 };
