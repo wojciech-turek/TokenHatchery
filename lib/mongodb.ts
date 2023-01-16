d89e98b267363950d1b4b65b81c74782e73a348a
wojciech-turek <contact@wojciechturek.com>
2023-01-16 10:22:25 +0100

    Cache mongoose connection

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,32 +1,15 @@
 import mongoose from "mongoose";
 
-if (!process.env.MONGODB_URI) {
-  throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
-}
-mongoose.set("strictQuery", false);
-const uri = process.env.MONGODB_URI;
-
 const connectMongo = async () => {
-  try {
-    await mongoose.connect(uri);
-  } catch (error) {
-    console.error(error);
+  if (!global.mongooseConnection) {
+    if (!process.env.MONGODB_URI) {
+      throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
+    }
+    mongoose.set("strictQuery", false);
+    global.mongooseConnection = await mongoose.connect(process.env.MONGODB_URI);
   }
-};
 
-mongoose.connection.on("connected", () => {
-  console.log("MongoDB connected");
-});
-mongoose.connection.on("error", (error) => {
-  console.log("MongoDB connection error: ", error);
-});
-mongoose.connection.on("disconnected", () => {
-  console.log("MongoDB disconnected");
-});
-process.on("SIGINT", async () => {
-  await mongoose.connection.close();
-  console.log("Mongoose connection closed due to NodeJs process termination");
-  process.exit(0);
-});
+  return global.mongooseConnection;
+};
 
 export default connectMongo;
