6ddffdf6f1a963612420fad20ddd6a986e11ec50
wojciech-turek <contact@wojciechturek.com>
2023-01-19 11:53:35 +0100

    Remove analytics to cut costs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,7 +20,6 @@ import { CoinbaseWalletConnector } from "wagmi/connectors/coinbaseWallet";
 import { LedgerConnector } from "wagmi/connectors/ledger";
 import { MetaMaskConnector } from "wagmi/connectors/metaMask";
 import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
-import { Analytics } from "@vercel/analytics/react";
 import { publicProvider } from "wagmi/providers/public";
 import Layout from "components/Layout/Layout";
 import Head from "next/head";
@@ -75,7 +74,6 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Analytics />
       <QueryClientProvider client={queryClient}>
         <ReactQueryDevtools initialIsOpen={false} />
         <WagmiConfig client={client}>
