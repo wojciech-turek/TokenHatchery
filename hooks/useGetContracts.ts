6455bac85d8055c865a32542c8c60f2108e79733
wojciech-turek <contact@wojciechturek.com>
2023-01-26 20:07:09 +0100

    Populate cache with contract data

--- hooks/useGetContracts.ts
+++ hooks/useGetContracts.ts
@@ -1,13 +1,24 @@
 import useApi from "hooks/useApi";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 
 export const useGetContracts = (address: string) => {
+  const queryClient = useQueryClient();
   const { getContractsByAddress } = useApi();
   return useQuery(
     ["contracts", address],
     () => getContractsByAddress(address),
     {
       staleTime: 1000 * 60 * 5,
+      onSuccess: (data) => {
+        data.forEach((contractType) => {
+          contractType.deployments.forEach((contract) => {
+            queryClient.setQueryData(["contract", contract._id], {
+              ...contract,
+              type: contractType.type,
+            });
+          });
+        });
+      },
     }
   );
 };
